0f2dab49489943967a099a181fe0932509ccb55c
sebastianwoelfle <holz.wurm1908@gmail.com>
2025-01-16 17:03:07 +0100

    test+pom

---
+++ src/test/java/ProjektNeu/FilmeTest.java
@@ -0,0 +1,18 @@
+package ProjektNeu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmeTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAusleihzeitraum() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
