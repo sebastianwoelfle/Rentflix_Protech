b2a446d7d281f33775bd4617886c3b383becdc29
sebastianwoelfle <holz.wurm1908@gmail.com>
2025-01-16 17:24:38 +0100

    test+pom1

--- src/test/java/ProjektNeu/FilmeTest.java
+++ src/test/java/ProjektNeu/FilmeTest.java
@@ -1,18 +1,36 @@
 package ProjektNeu;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilmeTest {
 
-    @org.junit.jupiter.api.Test
-    void getName() {
+    @Test
+    void testFilmeConstructor() {
+        Filme film = new Filme("Inception", "Sci-Fi", false, "3-Sterne", 7);
+        assertNotNull(film);
     }
 
-    @org.junit.jupiter.api.Test
-    void getAusleihzeitraum() {
+    @Test
+    void testGetName() {
+        Filme film = new Filme("Iron Man", "Sci-Fi", false, "2-Sterne", 9);
+        assertEquals("Iron Man", film.getName());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void testGetAusleihzeitraum() {
+        Filme film = new Filme("Avatar", "Sci-Fi", false, "3-Sterne", 10);
+        assertEquals(10, film.getAusleihzeitraum());
+    }
+
+    @Test
     void testToString() {
+        Filme film = new Filme("Gladiator", "Action", false, "3-Sterne", 3);
+        String result = film.toString();
+        assertTrue(result.contains("Name: Gladiator"));
+        assertTrue(result.contains("Genre: Action"));
+        assertTrue(result.contains("Ab 18: Nein"));
+        assertTrue(result.contains("Bewertung: 3-Sterne"));
+        assertTrue(result.contains("Ausleihzeitraum: 3"));
     }
 }
